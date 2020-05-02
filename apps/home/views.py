45c4407e5286bfac024f47668cc4fcc4838aa58c
Juan Muñoz <cl.jmunoz@gmail.com>
2020-05-02 06:46:06 -0400

    ajustes queryset

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from apps.home.models import advertisement, OpeningHours, SpecialDays
+from apps.evaluate.models import Ratings
 from datetime import datetime
+from django.db.models import Avg
 
 # Create your views here.
 
 class HomeList(ListView):
     model = advertisement
     template_name = 'home.html'
-    paginate_by = 6
+    #paginate_by = 6
 
     def get_context_data(self, **kwargs):
         context = super(HomeList,self).get_context_data(**kwargs)
@@ -16,8 +18,19 @@ class HomeList(ListView):
         now = datetime.now()
         data = []
         for adv in advertisements:
-            rating = 5 # Ratings.objects.filter(Schools=escuela.Id).aggregate(Avg('Score'))['Score__avg']
-            
+            OpH = 0
+            rating = Ratings.objects.filter(advertisementRating=adv.id).aggregate(Avg('rating_stars'))['rating_stars__avg']
+            countRatings = Ratings.objects.filter(advertisementRating=adv.id).count()
+            OpH = OpeningHours.objects.filter(store = adv.id, weekday_from = now.strftime("%w"))[1]
+            #HourFrom =OpeningHours.objects.filter(store = adv.id, weekday_from = now.strftime("%w"))[0].from_hour
+            # HourTo = OpeningHours.objects.filter(store = adv.id, weekday_from = now.strftime("%w")).value('to_hour')
+            #OpeningHours.from_hour
+            # print(OpH.from_hour)
+            # print(OpH.to_hour)
+            # print(now.strftime("%H"))
+            # if int(OpH.from_hour) <= int(now.strftime("%H")) and int(OpH.to_hour) >= int(now.strftime("%H")):
+            #     op = True
+            # else:
             op = False
             dic = {
             'id' : adv.id,
@@ -32,6 +45,7 @@ class HomeList(ListView):
             'open': op,
             'price_from' : adv.price_from, 
             'price_to' : adv.price_to,
+            'countRatings': countRatings
         }
             data.append(dic)
 
